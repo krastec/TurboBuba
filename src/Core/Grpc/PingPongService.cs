98685d8516d269b81d07c182a89ca62a16d305b0
krastec <krastec@gmail.com>
2025-12-07 15:38:36 +0100

    Solution refactoring

---
+++ src/Core/Grpc/PingPongService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using PingPong;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurboBuba.Grpc
+{
+    public class PingPongService : PingPong.PingPong.PingPongBase
+    {
+        public override Task<PongReply> SendPing(PingRequest request, ServerCallContext context)
+        {
+            var incoming = request.Message ?? "";
+            // можно логировать или делать что-то ещё
+            var reply = new PongReply { Message = "Pong: " + incoming };
+            return Task.FromResult(reply);
+        }
+    }
+}
